5fdd9461a6e6a3317d2a7561304a82ba15ee4a9c
ahmedmed7t <medhatahmed19.am9@gmail.com>
2019-04-27 19:55:50 +0200

    Medhat first Base edit

--- app/src/main/java/com/example/crazynet/medicalhub/AskDiscussActivity.java
+++ app/src/main/java/com/example/crazynet/medicalhub/AskDiscussActivity.java
@@ -18,6 +18,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class AskDiscussActivity extends AppCompatActivity {
@@ -41,6 +43,17 @@ public class AskDiscussActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ask_discuss);
+
+        Window window = getWindow();
+
+// clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+// finally change the color
+        window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
